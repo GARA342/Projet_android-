721e7f5f4b771e22139e71c67ac6f0d202673f55
zami <christophezami1@gmail.com>
2024-07-07 19:46:13 -0400

    message

--- app/src/main/java/com/example/l3mig2024/MainActivity.java
+++ app/src/main/java/com/example/l3mig2024/MainActivity.java
@@ -16,16 +16,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+        //Login et Mot de passe de connexion.
+        String Login ="christophe";
+        String password ="BZFTELUX234";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-     //   EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-      /*  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });*/
+   
 
         Button bt1 = (Button)findViewById(R.id.valider);
         bt1.setOnClickListener(this);
@@ -47,10 +46,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             EditText TextPass = (EditText)findViewById(R.id.pass);
 
 
-            Intent inte =new Intent(this, MainActivity2.class);
-            inte.putExtra("Nom", textLog.getText().toString());
-            inte.putExtra("prenom",  TextPass.getText().toString());
-            startActivity(inte);
+              String enteredLogin = textLog.getText().toString();
+            String enteredPassword = TextPass.getText().toString();
+
+            if (enteredLogin.equals(Login) && enteredPassword.equals(password)) {
+                Intent inte =new Intent(this, MainActivity2.class);
+                inte.putExtra("Nom", textLog.getText().toString());
+                inte.putExtra("prenom",  TextPass.getText().toString());
+                startActivity(inte);
+            }else {
+                //Affichage du message si le mot de passe et login sont incorrecte
+                Toast.makeText(this, "Login ou mot de passe incorrect", Toast.LENGTH_SHORT).show();
+            }
 
         }
         if(v.getId() == R.id.supprimer){
